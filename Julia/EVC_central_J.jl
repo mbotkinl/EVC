2a3b54f295a9689140dbc28f77947ba59f2a7e19
mbotkinl <32102213+mbotkinl@users.noreply.github.com>
2018-04-10 16:02:24 -0400

    more efficient julia obj function

--- Julia/EVC_central_J.jl
+++ Julia/EVC_central_J.jl
@@ -7,11 +7,11 @@ using JuMP
 using Gurobi
 #using Ipopt
 #using Clp
-using MAT #to read in scenarios from matlab 
+using MAT #to read in scenarios from matlab
 
 
 function string_as_varname(s::String,v::Any)
-	 s=Symbol(s) 
+	 s=Symbol(s)
 	 @eval (($s) = ($v))
 end
 
@@ -19,7 +19,7 @@ end
 file = matopen("EVCscenarioN10.mat")
 #file = matopen("test.mat")
 varnames=names(file)
-varNum=length(varnames)	
+varNum=length(varnames)
 varnamesS=collect(varnames)
 
 #println(varnamesS)
@@ -37,37 +37,47 @@ end
 close(file)
 println("done reading in")
 
+N=convert(Int,N)
+K=convert(Int,K)
+S=convert(Int,S)
+
 
 #initialize
 xi=x0
 
-#setup model 
-n1=convert(Int,N*(K+1))
-n2=convert(Int,(N+1)*(K+1))
-n3=convert(Int,S*(K+1))
+#setup model
+#n1=convert(Int,N*(K+1))
+#n2=convert(Int,(N+1)*(K+1))
+#n3=convert(Int,S*(K+1))
 
 println("setting up model")
-m = Model(solver=GurobiSolver())
+m = Model(solver = GurobiSolver())
 #m = Model(solver = ClpSolver())
 #m = Model(solver = IpoptSolver())
 
-@variable(m,u[1:n1])
-@variable(m,x[1:n2])
-@variable(m,z[1:n3])
+@variable(m,u[1:N*(K+1)])
+@variable(m,x[1:(N+1)*(K+1)])
+@variable(m,z[1:S*(K+1)])
 
 println("obj")
 #@objective(m,Min,sum(u)) #fix???
 #@objective(m,Min,transpose(u)*Rt*u+transpose(x)*Qt*x-2*ones(1,n2)*Qt*x)
-@objective(m,Min,sum(u'*Rt*u+x'*Qt*x-2*ones(1,n2)*Qt*x))
 
+#@objective(m,Min,sum(u'*Rt*u+x'*Qt*x-2*ones(1,n2)*Qt*x))
+#@NLobjective(m,Min,(u'*Rt*u+x'*Qt*x-2*ones(1,n2)*Qt*x))
+
+
+objFun(x,u)=sum(sum((x[(k-1)*(N+1)+n,1]-1)^2*Qsi[n] for n=1:N) for k=1:K)+sum(sum((u[(k-1)*N+n,1])^2*Ri[n] for n=1:N) for k=1:(K-1))
+
+@objective(m,Min, objFun(x,u))
 
 println("constraints")
 @constraint(m,(eye(n2)-Ahat)*x.==A0hat*xi+Bhat*u+Vhat*w+Ehat*z)
 @constraint(m,0.==Hhat*u+Ghat*w+Fhat*z)
-@constraint(m,x.<=repmat([ones(N,1);Tmax],convert(Int,K+1),1))
+@constraint(m,x.<=repmat([ones(N,1);Tmax],K+1,1))
 @constraint(m,x.>=0) #add target???
-@constraint(m,u.<=repmat(imax,convert(Int,K+1),1))
-@constraint(m,u.>=repmat(imin,convert(Int,K+1),1))
+@constraint(m,u.<=repmat(imax,K+1,1))
+@constraint(m,u.>=repmat(imin,K+1,1))
 @constraint(m,z.>=0)
 @constraint(m,z.<=deltaI)
 
@@ -78,28 +88,30 @@ status = solve(m)
 
 xRaw=getvalue(x)
 xPlot=DataFrame()
-for ii= 1:convert(Int,N)
-	xPlot[:,ii]=xRaw[ii:convert(Int,N+1):length(xRaw)]
+for ii= 1:N
+	xPlot[:,ii]=xRaw[ii:N+1:length(xRaw)]
 end
 
-p1=plot(xPlot,x=Row.index,y=Col.value,color=Col.index,Geom.line,
+#p1=
+plot(xPlot,x=Row.index,y=Col.value,color=Col.index,Geom.line,
 		Guide.xlabel("Time"), Guide.ylabel("SOC"))
-    
-img=SVG("SOC.svg",6inch,4inch)
-draw(img,p1)
+#gui()
+#img=SVG("SOC.svg",6inch,4inch)
+#draw(img,p1)
 
 
 uRaw=getvalue(u)
 uPlot=DataFrame()
-for ii= 1:convert(Int,N)
-	uPlot[:,ii]=uRaw[ii:convert(Int,N):length(uRaw)]
+for ii= 1:N
+	uPlot[:,ii]=uRaw[ii:N:length(uRaw)]
 end
 
-p2=plot(uPlot,x=Row.index,y=Col.value,color=Col.index,Geom.line,
+#p2=
+plot(uPlot,x=Row.index,y=Col.value,color=Col.index,Geom.line,
 		Guide.xlabel("Time"), Guide.ylabel("PEV Current"))
-	
-img=SVG("Current.svg",6inch,4inch)
-draw(img,p2)
+
+#img=SVG("Current.svg",6inch,4inch)
+#draw(img,p2)
 
 
 
