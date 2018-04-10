bf2dcf21af1ffd4e6f9a299b86834599749061d6
mbotkinl <32102213+mbotkinl@users.noreply.github.com>
2018-04-10 17:21:10 -0400

    working EVC central in Julia

--- Julia/EVC_central_J.jl
+++ Julia/EVC_central_J.jl
@@ -1,5 +1,9 @@
-#remove converts once julia scenario
 
+
+
+#remove converts once julia scenario ???
+
+println("Loading Packages...")
 
 using Gadfly
 using DataFrames
@@ -8,7 +12,11 @@ using Gurobi
 #using Ipopt
 #using Clp
 using MAT #to read in scenarios from matlab
+using Cairo #for png output
+using Fontconfig
 
+
+println("Reading in Data...")
 
 function string_as_varname(s::String,v::Any)
 	 s=Symbol(s)
@@ -30,7 +38,7 @@ for i =1:varNum
 	if n!="MCOS"
 		string_as_varname(n,read(file,n))
 		#if isa(n,Array)
-		#	n=convert(DataFrame, n)
+		#	n=convert(DataFrame, n) #fix this???
 		#end
 	end
 end
@@ -40,15 +48,13 @@ println("done reading in")
 N=convert(Int,N)
 K=convert(Int,K)
 S=convert(Int,S)
-
+Kn=convert(Array{Int,2},Kn)
 
 #initialize
 xi=x0
 
-#setup model
-#n1=convert(Int,N*(K+1))
-#n2=convert(Int,(N+1)*(K+1))
-#n3=convert(Int,S*(K+1))
+#add mpc loop here ??
+i=1
 
 println("setting up model")
 m = Model(solver = GurobiSolver())
@@ -59,23 +65,25 @@ m = Model(solver = GurobiSolver())
 @variable(m,x[1:(N+1)*(K+1)])
 @variable(m,z[1:S*(K+1)])
 
+#desired SOC
+target=zeros((N+1)*(K+1),1);
+for ii=1:N
+   cur=Kn[ii]-(i-1);
+   ind=max(0,(cur-1)*(N+1))+ii:N+1:length(target);
+   target[ind]=Sn[ii];
+end
+
 println("obj")
-#@objective(m,Min,sum(u)) #fix???
-#@objective(m,Min,transpose(u)*Rt*u+transpose(x)*Qt*x-2*ones(1,n2)*Qt*x)
-
 #@objective(m,Min,sum(u'*Rt*u+x'*Qt*x-2*ones(1,n2)*Qt*x))
-#@NLobjective(m,Min,(u'*Rt*u+x'*Qt*x-2*ones(1,n2)*Qt*x))
-
-
-objFun(x,u)=sum(sum((x[(k-1)*(N+1)+n,1]-1)^2*Qsi[n] for n=1:N) for k=1:K)+sum(sum((u[(k-1)*N+n,1])^2*Ri[n] for n=1:N) for k=1:(K-1))
-
+objFun(x,u)=sum(sum((x[(k-1)*(N+1)+n,1]-1)^2*Qsi[n] for n=1:N+1) for k=1:K)+
+			sum(sum((u[(k-1)*N+n,1])^2*Ri[n]        for n=1:N) for k=1:K)
 @objective(m,Min, objFun(x,u))
 
 println("constraints")
-@constraint(m,(eye(n2)-Ahat)*x.==A0hat*xi+Bhat*u+Vhat*w+Ehat*z)
+@constraint(m,(eye((N+1)*(K+1))-Ahat)*x.==A0hat*xi+Bhat*u+Vhat*w+Ehat*z) #this is slow???
 @constraint(m,0.==Hhat*u+Ghat*w+Fhat*z)
 @constraint(m,x.<=repmat([ones(N,1);Tmax],K+1,1))
-@constraint(m,x.>=0) #add target???
+@constraint(m,x.>=target)
 @constraint(m,u.<=repmat(imax,K+1,1))
 @constraint(m,u.>=repmat(imin,K+1,1))
 @constraint(m,z.>=0)
@@ -85,37 +93,37 @@ println("solving....")
 status = solve(m)
 
 
-
+println("plotting....")
 xRaw=getvalue(x)
 xPlot=DataFrame()
 for ii= 1:N
-	xPlot[:,ii]=xRaw[ii:N+1:length(xRaw)]
+	xPlot[:,ii]=xRaw[collect(ii:N+1:length(xRaw))]
 end
 
-#p1=
-plot(xPlot,x=Row.index,y=Col.value,color=Col.index,Geom.line,
+p1=plot(xPlot,x=Row.index,y=Col.value,color=Col.index,Geom.line,
 		Guide.xlabel("Time"), Guide.ylabel("SOC"))
-#gui()
-#img=SVG("SOC.svg",6inch,4inch)
-#draw(img,p1)
+display(p1)
+draw(PNG("SOC.png", 4inch, 3inch), p1)
 
 
 uRaw=getvalue(u)
 uPlot=DataFrame()
 for ii= 1:N
-	uPlot[:,ii]=uRaw[ii:N:length(uRaw)]
+	uPlot[:,ii]=uRaw[collect(ii:N:length(uRaw))]
 end
 
-#p2=
-plot(uPlot,x=Row.index,y=Col.value,color=Col.index,Geom.line,
+p2=plot(uPlot,x=Row.index,y=Col.value,color=Col.index,Geom.line,
 		Guide.xlabel("Time"), Guide.ylabel("PEV Current"))
-
-#img=SVG("Current.svg",6inch,4inch)
-#draw(img,p2)
-
+display(p2)
+draw(PNG("Current.png", 4inch, 3inch), p2)
 
 
-#getvalue(u)
+
+p3=plot(x=1:K+1,y=xRaw[N+1:N+1:length(xRaw)],Geom.line,
+	Guide.xlabel("Time"), Guide.ylabel("Xfrm Temp (K)"),)
+display(p3)
+draw(PNG("Temp.png", 4inch, 3inch), p3)
+
 
 #pwd()
 #cd(homedir())
