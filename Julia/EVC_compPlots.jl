3e26ad15d61a8365025cf57196b4702b14afeb8f
mbotkinl <32102213+mbotkinl@users.noreply.github.com>
2018-06-15 12:10:54 +0200

    update comparison plots for ALAD

--- Julia/EVC_compPlots.jl
+++ Julia/EVC_compPlots.jl
@@ -1,14 +1,15 @@
 #comparison plots
-
+#need to confirm all metrics are the same equation
 
 #compare convergence rates
-legendLab=["Dual Ascent", "Fast Ascent", "ADMM"]
-legendCol=["blue","red", "green"]
+legendLab=["Dual Ascent", "Fast Ascent", "ADMM","ALADIN"]
+legendCol=["blue","red", "green","orange"]
 
 #f-fStar
 fgapPlotComp=plot(layer(x=1:convIt,y=fConvDual_dualAscent[1:convIt,1],Geom.line,Theme(default_color=colorant"blue")),
             layer(x=1:convIt,y=fConvDual_fastAscent[1:convIt,1],Geom.line,Theme(default_color=colorant"red")),
             layer(x=1:convIt,y=fConvADMM[1:convIt,1],Geom.line,Theme(default_color=colorant"green")),
+            layer(x=1:convIt,y=fConvALAD[1:convIt,1],Geom.line,Theme(default_color=colorant"orange")),
 			Guide.xlabel("Iteration"), Guide.ylabel("2-Norm Objective Function Gap",orientation=:vertical),Scale.y_log10,
 			Coord.Cartesian(xmin=0,xmax=convIt),Theme(background_color=colorant"white",major_label_font_size=30pt,line_width=2pt,
 			minor_label_font_size=26pt,key_label_font_size=26pt),
@@ -18,6 +19,7 @@ fgapPlotComp=plot(layer(x=1:convIt,y=fConvDual_dualAscent[1:convIt,1],Geom.line,
 unPlotComp=plot(layer(x=1:convIt,y=unConvDual_dualAscent[1:convIt,1],Geom.line,Theme(default_color=colorant"blue")),
             layer(x=1:convIt,y=unConvDual_fastAscent[1:convIt,1],Geom.line,Theme(default_color=colorant"red")),
             layer(x=1:convIt,y=unConvADMM[1:convIt,1],Geom.line,Theme(default_color=colorant"green")),
+            layer(x=1:convIt,y=unConvALAD[1:convIt,1],Geom.line,Theme(default_color=colorant"orange")),
 			Guide.xlabel("Iteration"), Guide.ylabel("2-Norm EV Current Gap",orientation=:vertical),Scale.y_log10,
 			Coord.Cartesian(xmin=0,xmax=convIt),Theme(background_color=colorant"white",major_label_font_size=30pt,line_width=2pt,
 			minor_label_font_size=26pt,key_label_font_size=26pt),
@@ -26,16 +28,18 @@ unPlotComp=plot(layer(x=1:convIt,y=unConvDual_dualAscent[1:convIt,1],Geom.line,T
 
 #Ax-b=0
 constPlotComp=plot(layer(x=1:convIt,y=constConvDual[1:convIt,1],Geom.line,Theme(default_color=colorant"blue")),
-            layer(x=1:convIt,y=constConvADMM[1:convIt,1],Geom.line,Theme(default_color=colorant"green")),
+                   layer(x=1:convIt,y=constConvADMM[1:convIt,1],Geom.line,Theme(default_color=colorant"green")),
+                   layer(x=1:convIt,y=constConvALAD[1:convIt,1],Geom.line,Theme(default_color=colorant"orange")),
 			Guide.xlabel("Iteration"), Guide.ylabel("2-Norm Ax Gap",orientation=:vertical),Scale.y_log10,
 			Coord.Cartesian(xmin=0,xmax=convIt),Theme(background_color=colorant"white",major_label_font_size=30pt,line_width=2pt,
 			minor_label_font_size=26pt,key_label_font_size=26pt),
-            Guide.manual_color_key("", ["Dual Ascent", "ADMM"], ["blue", "green"]))
+            Guide.manual_color_key("", ["Dual Ascent", "ADMM","ALADIN"], ["blue", "green","orange"]))
 
 #lambda-lamstar
 lamPlotComp=plot(layer(x=1:convIt,y=ConvDual_dualAscent[1:convIt,1],Geom.line,Theme(default_color=colorant"blue")),
             layer(x=1:convIt,y=ConvDual_fastAscent[1:convIt,1],Geom.line,Theme(default_color=colorant"red")),
             layer(x=1:convIt,y=ConvADMM[1:convIt,1],Geom.line,Theme(default_color=colorant"green")),
+            layer(x=1:convIt,y=ConvALAD[1:convIt,1],Geom.line,Theme(default_color=colorant"orange")),
 			Guide.xlabel("Iteration"), Guide.ylabel("2-Norm Lambda Gap",orientation=:vertical),Scale.y_log10,
 			Coord.Cartesian(xmin=0,xmax=convIt),Theme(background_color=colorant"white",major_label_font_size=30pt,line_width=2pt,
 			minor_label_font_size=26pt,key_label_font_size=26pt),
@@ -45,6 +49,7 @@ lamPlotComp=plot(layer(x=1:convIt,y=ConvDual_dualAscent[1:convIt,1],Geom.line,Th
 itPlotComp=plot(layer(x=1:convIt,y=itConvDual_dualAscent[1:convIt,1],Geom.line,Theme(default_color=colorant"blue")),
             layer(x=1:convIt,y=itConvDual_fastAscent[1:convIt,1],Geom.line,Theme(default_color=colorant"red")),
             layer(x=1:convIt,y=itConvADMM[1:convIt,1],Geom.line,Theme(default_color=colorant"green")),
+            layer(x=1:convIt,y=itConvALAD[1:convIt,1],Geom.line,Theme(default_color=colorant"orange")),
 			Guide.xlabel("Iteration"), Guide.ylabel("2-Norm Lambda Iterate Gap",orientation=:vertical),Scale.y_log10,
 			Coord.Cartesian(xmin=0,xmax=convIt),Theme(background_color=colorant"white",major_label_font_size=30pt,line_width=2pt,
 			minor_label_font_size=26pt,key_label_font_size=26pt),
