8ef7a56ecab8b4fe5109688e304df082f415027a
mbotkinl <32102213+mbotkinl@users.noreply.github.com>
2018-06-18 17:24:08 +0200

    renaming

--- Julia/EVCcentral.jl
+++ Julia/EVCcentral.jl
@@ -9,16 +9,6 @@ xt0=T0
 stepI=1
 horzLen=K1
 
-println("setting up model")
-m = Model(solver = GurobiSolver(Presolve=0,BarHomogeneous=1,NumericFocus=3))
-#m = Model(solver = IpoptSolver())
-
-#u w and z are one index ahead of x. i.e the x[k+1]=x[k]+eta*u[k+1]
-@variable(m,u[1:N*(horzLen+1)])
-@variable(m,sn[1:(N)*(horzLen+1)])
-@variable(m,xt[1:(horzLen+1)])
-@variable(m,z[1:S*(horzLen+1)])
-
 #desired SOC
 target=zeros(N*(horzLen+1),1);
 for ii=1:N
@@ -27,31 +17,42 @@ for ii=1:N
    target[ind]=Snmin[ii,1]
 end
 
+
+println("setting up model")
+centralModel = Model(solver = GurobiSolver(Presolve=0,BarHomogeneous=1,NumericFocus=3))
+#m = Model(solver = IpoptSolver())
+
+#u w and z are one index ahead of x. i.e the x[k+1]=x[k]+eta*u[k+1]
+@variable(centralModel,u[1:N*(horzLen+1)])
+@variable(centralModel,sn[1:(N)*(horzLen+1)])
+@variable(centralModel,xt[1:(horzLen+1)])
+@variable(centralModel,z[1:S*(horzLen+1)])
+
 println("obj")
 objFun(sn,xt,u)=sum(sum((sn[(k-1)*(N)+n,1]-1)^2*Qsi[n,1]     for n=1:N) for k=1:horzLen+1) +
 				sum((xt[k,1]-1)^2*Qsi[N+1,1]                 for k=1:horzLen+1) +
 				sum(sum((u[(k-1)*N+n,1])^2*Ri[n,1]           for n=1:N) for k=1:horzLen+1)
-@objective(m,Min, objFun(sn,xt,u))
+@objective(centralModel,Min, objFun(sn,xt,u))
 
 println("constraints")
-@constraint(m,stateCon1,sn[1:N,1].==sn0[1:N,1]+ηP[:,1].*u[1:N,1])
-@constraint(m,stateCon2[k=1:horzLen,n=1:N],sn[n+(k)*(N),1]==sn[n+(k-1)*(N),1]+ηP[n,1]*u[n+(k)*(N),1])
-@constraint(m,tempCon1,xt[1,1]==τP*xt0+γP*deltaI*sum((2*s-1)*z[s,1] for s=1:S)+ρP*w[stepI*2,1])
-@constraint(m,tempCon2[k=1:horzLen],xt[k+1,1]==τP*xt[k,1]+γP*deltaI*sum((2*s-1)*z[k*S+s,1] for s=1:S)+ρP*w[stepI*2+k*2,1])
-@constraint(m,currCon[k=1:horzLen+1],0==-sum(u[(k-1)*(N)+n] for n=1:N)-w[(k-1)*2+1]+sum(z[(k-1)*(S)+s] for s=1:S))
-@constraint(m,sn.<=1)
-@constraint(m,sn.>=target)
+@constraint(centralModel,stateCon1,sn[1:N,1].==sn0[1:N,1]+ηP[:,1].*u[1:N,1])
+@constraint(centralModel,stateCon2[k=1:horzLen,n=1:N],sn[n+(k)*(N),1]==sn[n+(k-1)*(N),1]+ηP[n,1]*u[n+(k)*(N),1])
+@constraint(centralModel,tempCon1,xt[1,1]==τP*xt0+γP*deltaI*sum((2*s-1)*z[s,1] for s=1:S)+ρP*w[stepI*2,1])
+@constraint(centralModel,tempCon2[k=1:horzLen],xt[k+1,1]==τP*xt[k,1]+γP*deltaI*sum((2*s-1)*z[k*S+s,1] for s=1:S)+ρP*w[stepI*2+k*2,1])
+@constraint(centralModel,currCon[k=1:horzLen+1],0==-sum(u[(k-1)*(N)+n] for n=1:N)-w[(k-1)*2+1]+sum(z[(k-1)*(S)+s] for s=1:S))
+@constraint(centralModel,sn.<=1)
+@constraint(centralModel,sn.>=target)
 if noTlimit==0
-	@constraint(m,upperTCon,xt.<=Tmax)
+	@constraint(centralModel,upperTCon,xt.<=Tmax)
 end
-@constraint(m,xt.>=0)
-@constraint(m,upperCCon,u.<=repmat(imax,horzLen+1,1))
-@constraint(m,u.>=repmat(imin,horzLen+1,1))
-@constraint(m,z.>=0)
-@constraint(m,z.<=deltaI)
+@constraint(centralModel,xt.>=0)
+@constraint(centralModel,upperCCon,u.<=repmat(imax,horzLen+1,1))
+@constraint(centralModel,u.>=repmat(imin,horzLen+1,1))
+@constraint(centralModel,z.>=0)
+@constraint(centralModel,z.<=deltaI)
 
 println("solving....")
-statusM = solve(m)
+statusM = solve(centralModel)
 if statusM!=:Optimal
 	@printf "Failed %s \n" statusM
     return
@@ -92,7 +93,7 @@ else
     snStar=snRaw
 	uStar=uRaw
 	zStar=zRaw
-    fStar=getobjectivevalue(m)
+    fStar=getobjectivevalue(centralModel)
     lamTempStar=lambdaUpperT
 	lamCurrStar=lambdaCurr
 
@@ -102,8 +103,6 @@ else
 		zSumStar[k,1]=sum(zStar[(k-1)*(S)+s,1] for s=1:S)
         uSumStar[k,1]=sum(uStar[(k-1)*N+n,1] for n=1:N)
 	end
-
-
 
 	println("plotting....")
 	xPlot=zeros(horzLen+1,N)
