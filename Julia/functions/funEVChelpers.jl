706bc0f433a0ee2d0e12fb6f10fa91ae59844c02
mbotkinl <32102213+mbotkinl@users.noreply.github.com>
2018-09-23 22:10:59 -0400

    Add compareRuns helper func

--- Julia/functions/funEVChelpers.jl
+++ Julia/functions/funEVChelpers.jl
@@ -1,4 +1,6 @@
 # helper functions for EVC code
+using JLD
+using DataFrames
 
 function saveRun(path::String, filename::String, time::Float64, scenario::scenarioStruct, solution, convMetrics=convMetricsStruct(), convIt=0)
     JLD.save(path*filename*".jld","time", time, "scenario", scenario, "solution", solution,
@@ -8,4 +10,23 @@ end
 function clips()
     t=clipboard()
     return t[2:length(t)-1]
+end
+
+
+function compareRuns(path)
+    files = filter(x->contains(x,"_"), readdir(path))
+
+    compareTable = DataFrame(name=String[],time=Float64[],cLamDiff=Float64[],lamDiff=Float64[],
+    cObjDiff=Float64[],objDiff=Float64[])
+    for file in files
+        loadF=JLD.load(path*file)
+        timeT=loadF["time"]
+        cm=loadF["convMetrics"]
+        convIt=loadF["convIt"]
+        ind= if convIt>0 convIt-1 else length(cm.lam) end
+        stats = [file timeT minimum(cm.lam[1:ind]) cm.lam[ind-1]-cm.lam[ind-2] minimum(cm.objVal[1:ind]) cm.objVal[ind-1]-cm.objVal[ind-2]]
+        push!(compareTable,stats)
+    end
+
+    return compareTable
 end
