ca2464ad294e516c5b3c4e407a5c6a7310b2900c
mbotkinl <32102213+mbotkinl@users.noreply.github.com>
2018-12-15 21:09:51 -0500

    functionalize PEM

--- Julia/EVC_PEM.jl
+++ Julia/EVC_PEM.jl
@@ -4,7 +4,6 @@ using Gurobi
 #pull out a few key variables
 N=evS.N
 S=evS.S
-horzLen=evS.K1
 
 include("C://Users//micah//Documents//uvm//Research//EVC code//Julia//functions//funEVCpem.jl")
 
@@ -17,27 +16,27 @@ if loadResults
 	pemSol=loadF["solution"]
 else
 	println("Running PEM Sim")
-	timeT=@elapsed pemSol,ratio=pemEVC(N,S,horzLen,evS,forecastError,slack)
-	timeT=timeT/horzLen
+	timeT=@elapsed pemSol=pemEVC(evS,slack,silent)
+	timeT=timeT/evS.K
 	if saveResults saveRun(path,fname,timeT, evS,pemSol) end
 end
 
-p1pem=plot(pemSol.Sn,xlabel="Time",ylabel="PEV SOC",legend=false,xlims=(0,horzLen+1),ylims=(0,1))
+p1pem=plot(pemSol.Sn,xlabel="Time",ylabel="PEV SOC",legend=false,xlims=(0,evS.K),ylims=(0,1))
 if drawFig savefig(p1pem,path*"J_PEM_SOC.png") end
 
-p2pem=plot(pemSol.Un,xlabel="Time",ylabel="PEV Current (kA)",legend=false,xlims=(0,horzLen+1))
+p2pem=plot(pemSol.Un,xlabel="Time",ylabel="PEV Current (kA)",legend=false,xlims=(0,evS.K))
 if drawFig savefig(p2pem,path*"J_PEM_Curr.png") end
 
-p3pem=plot(1:horzLen+1,pemSol.Xt,label="XFRM Temp",xlims=(0,horzLen+1),xlabel="Time",ylabel="Temp (K)")
-plot!(p3pem,1:horzLen+1,evS.Tmax*ones(horzLen+1),label="XFRM Limit",line=(:dash,:red))
+p3pem=plot(pemSol.Tactual*1000,label="XFRM Temp",xlims=(0,evS.K),xlabel="Time",ylabel="Temp (K)")
+plot!(p3pem,evS.Tmax*ones(evS.K)*1000,label="XFRM Limit",line=(:dash,:red))
 if drawFig savefig(p3pem,path*"J_PEM_Temp.png") end
 
 # pRpem=plot(ratio,xlabel="Time",ylabel="Ratio",xlims=(0,horzLen+1),legend=false)
 # if drawFig savefig(pRpem,path*"J_PEM_Ratio.png") end
 
 
-aggUpem=plot(1:horzLen+1,hcat(sum(uPlot[:,i] for i=1:N),pemSol.uSum),label=["Central" "PEM"],
-			xlims=(0,horzLen+1),xlabel="Time",ylabel="PEV Current (kA)")
+aggUpem=plot(hcat(cSol.uSum,pemSol.uSum),label=["Central" "PEM"],
+			xlims=(0,evS.K),xlabel="Time",ylabel="PEV Current (kA)")
 if drawFig savefig(aggUpem,path*"J_PEM_uSum.png") end
 
 
--- Julia/functions/funEVCpem.jl
+++ Julia/functions/funEVCpem.jl
@@ -1,162 +1,145 @@
 # run PEM
 
-function pemEVC(N::Int,S::Int,horzLen::Int,evS::scenarioStruct,forecastError::Bool, slack::Bool)
-
-	if forecastError
-		iD=evS.iDnoise
-	else
-		iD=evS.iD
-	end
-
-	sn=zeros(horzLen+1,N)
-	un=zeros(horzLen+1,N)
-	uSum=zeros(horzLen+1,1)
-	Itotal=zeros(horzLen+1,1)
-	ratio=zeros(horzLen+1,N)
-	mu=zeros(horzLen+1,N)
-	P=zeros(horzLen+1,N)
-	Req=zeros(horzLen+1,N)
-	Rec=zeros(horzLen+1,N)
-	T=zeros(horzLen+1,1)
-
-	# sn[1:N]=evS.s0
-	# T[1]=evS.t0
+function pemEVClocal(n,stepI,horzLen,packLen,evS,pemSol)
 	epsilon=1e-3
-	packLen=3 #number of time steps
 	mttr=300
-	poolSol=Int(min(round(N*packLen),10))
 
-	for k =1:horzLen+1
-		#println(k)
-		#send requests
-	    for n=1:N
-			# clean up this logic***
-			if k==1
-				existPack=false
-			elseif (un[k-1,n]>0)
-				if k<=packLen
-					prevChar=k-1
-					existPack=true
-				elseif (un[k-(packLen),n]==0)
-					prevChar=sum(un[(k-1):(k-(packLen)),n])
-					existPack=true
-				else
-					existPack=false
-				end
-			else
-				existPack=false
-			end
-
-			if existPack==true # still using a packet
-				#ratio[k,n]=-1
-				Req[k,n]=-packLen+prevChar
-			else
-				desiredSOC=1 # for now
-				setSOC=0.5 # for now
-				# desiredSOC=evS.Snmin[n] #this should be a ratio?
-				prevSOC= if k>1 sn[k-1,n] else evS.s0[n] end
-		        if (prevSOC-desiredSOC)>=-epsilon #desiredSOC satisfied
-					ratio[k,n]=0
-					Req[k,n]=0
-					#elseif k>=evS.Kn[n] # opt out (not satisified)
-		        else
-					ratio[k,n]=(desiredSOC-prevSOC)/(evS.ηP[n]*evS.imax[n]*(evS.Kn[n]-(k-1)))
-					if ratio[k,n]>=1 # opt out (need to charge for rest of time)
-						ratio[k,n]=1
-						Req[k,n]=-packLen
-					else
-						#mu[k,n] = 1/mttr*((desiredSOC-ratio[k,n])/(ratio[k,n]-0))*((setSOC-0)/(desiredSOC-setSOC))
-						mu[k,n] = 1/mttr*((ratio[k,n]-0)/(desiredSOC-ratio[k,n]))*((desiredSOC-setSOC)/(setSOC-0))
-						#mu[k,n] = ratio[k,n]
-						P[k,n] = min(max(1-exp(-mu[k,n]*evS.Ts),0),1)
-						t=rand()
-					  	Req[k,n]=if (t>(1-P[k,n])) 1 else  0  end
-					end
-		        end
-			end
-		end
-
-
-
-		#receive requests and forecast for the next packLen intervals
-		prevT = if k>1 T[k-1] else evS.t0 end
-	    requiredCh=Int.(Req[k,:].<0)
-		#requiredCh=Int.(ratio[k,:].>=1)
-		requiredInd=findall(x->x==1,requiredCh)
-		optOff=findall(x->x==0,Req[k,:])
-
-		# Test=zeros(packLen,1)
-		# Iest=sum(abs(Req[k,n])*evS.imax[n] for n=1:N)
-		# Test[1] = evS.τP*prevT+evS.γP*(Iest+evS.iD[k])^2+evS.ρP*evS.Tamb[k]
-		# for kk=2:packLen
-		# 	Test[kk] = evS.τP*Test[kk-1]+evS.γP*(Iest+evS.iD[kk])^2+evS.ρP*evS.Tamb[k+kk]
-		# end
-		#for now
-		# if all(Test.<=evS.Tmax)
-		# 	Rec[k,:]=abs.(Req[k,:])
-		# else # accept the only the opt outs aka -1
-		# 	Rec[k,:]=Int.(Req[k,:].<0)
-		# end
-
-		m = Model(solver = GurobiSolver(PoolSearchMode=1,PoolSolutions=poolSol,PoolGap=0))
-		@variable(m,u[1:packLen,1:N],Bin)
-		@variable(m,Test[1:packLen])
-		@objective(m,Min,-sum(u))
-		@constraint(m,tempCon1,Test[1]>=evS.τP*prevT+evS.γP*(sum(u[1,n]*evS.imax[n] for n=1:N)+iD[k])^2+evS.ρP*evS.Tamb[k])
-		@constraint(m,tempCon2[kk=2:packLen],Test[kk]>=evS.τP*Test[kk-1]+evS.γP*(sum(u[kk,n]*evS.imax[n] for n=1:N)+iD[k+kk])^2+evS.ρP*evS.Tamb[k+kk])
-		@constraint(m,Test.<=evS.Tmax)
-		# @constraint(m,optOn[kk=1:packLen],sum(u[n,kk] for n in requiredInd[kk])==length(requiredInd[kk]))
-		@constraint(m,optOnC[nn=1:length(requiredInd)],sum(u[kk,requiredInd[nn]] for kk=1:Int(abs(Req[k,requiredInd[nn]])))==abs(Req[k,requiredInd[nn]]))
-		@constraint(m,optOffC[kk=1:packLen],sum(u[kk,n] for n in optOff)==0)
-
-		TT = stdout
-		redirect_stdout()
-		statusC = solve(m)
-		redirect_stdout(TT)
-
-
-		# m = Model(solver = GurobiSolver())
-		# @variable(m,u[1:5],Bin)
-		# @objective(m,Min,-sum(u))
-		# @constraint(m,sum(u)==3)
-		# statusC = solve(m)
-	    # getvalue(u)
-		#
-
-
-		if statusC==:Optimal
-			#take random solution if multiple
-			solCount=Gurobi.get_sol_count(getrawsolver(m))
-			if solCount>1
-				println(solCount," multiple solutions found")
-				solNum=rand(1:solCount-1)
-				setparam!(getrawsolver(m),"SolutionNumber",solNum)
-				getparam(getrawsolver(m),"SolutionNumber")
-				sol=Gurobi.get_dblattrarray(getrawsolver(m),"Xn",1,N)
-			else
-				sol=getvalue(u)[1,:]
-			end
-			Rec[k,:]=sol
+	# clean up this logic***
+	if stepI==1
+		existPack=false
+	elseif (pemSol.Un[stepI-1,n]>0)
+		if stepI<=packLen
+			prevChar=stepI-1
+			existPack=true
+		elseif (pemSol.Un[stepI-(packLen),n]==0)
+			prevChar=sum(pemSol.Un[(stepI-1):(stepI-(packLen)),n])
+			existPack=true
 		else
-			#return
-			Rec[k,:]=requiredCh
+			existPack=false
 		end
-
-		#R>=1 and packRec get charged
-		for n=1:N
-			un[k,n]= if Rec[k,n]==1 evS.imax[n] else  0  end
-			prevSOC= if k>1 sn[k-1,n] else evS.s0[n] end
-			sn[k,n]=prevSOC+evS.ηP[n]*un[k,n]
-		end
-
-		# actual dynamics
-		uSum[k] = sum(un[k,n] for n=1:N)
-		Itotal[k] = uSum[k] + evS.iD[k]
-		T[k] = evS.τP*prevT+evS.γP*Itotal[k]^2+evS.ρP*evS.Tamb[k]
+	else
+		existPack=false
 	end
 
-	objVal=sum(sum((sn[k,n]-1)^2*evS.Qsi[n,1]+(un[k,n])^2*evS.Ri[n,1] for n=1:N) for k=1:(horzLen+1))
-	pemSol=centralSolutionStruct(Xt=T,Tactual=T,Un=un,Sn=sn, Itotal=Itotal,uSum=uSum,objVal=objVal)
+	if existPack==true # still using a packet
+		Req=-packLen+prevChar
+	else
+		desiredSOC=1 # for now
+		setSOC=0.5 # for now
+		# desiredSOC=evS.Snmin[n] #this should be a ratio?
+		prevSOC= if stepI>1 pemSol.Sn[stepI-1,n] else evS.s0[n] end
+		if (prevSOC-desiredSOC)>=-epsilon #desiredSOC satisfied
+			ratio=0
+			Req=0
+		else
+			ratio=(desiredSOC-prevSOC)/(evS.ηP[n]*evS.imax[n]*(evS.Kn[n]-(stepI-1)))
+			if ratio>=1 # opt out (need to charge for rest of time)
+				ratio=1
+				Req=-packLen
+			else
+				#mu[stepI,n] = 1/mttr*((desiredSOC-ratio[stepI,n])/(ratio[stepI,n]-0))*((setSOC-0)/(desiredSOC-setSOC))
+				mu = 1/mttr*((ratio-0)/(desiredSOC-ratio))*((desiredSOC-setSOC)/(setSOC-0))
+				P = min(max(1-exp(-mu*evS.Ts),0),1)
+				t=rand()
+				Req=if (t>(1-P)) 1 else  0  end
+			end
+		end
+	end
+	#return ratio,mu,P,Req
+	return Req
+end
 
-	return pemSol, ratio
+function pemEVCcoord(stepI,horzLen,packLen,evS,pemSol,Req)
+	poolSol=Int(min(round(N*packLen),100))
+
+	#receive requests and forecast for the next packLen intervals
+	prevT = if stepI>1 pemSol.Tactual[stepI-1] else evS.t0 end
+    requiredCh=Int.(Req[stepI,:].<0)
+	#requiredCh=Int.(ratio[stepI,:].>=1)
+	requiredInd=findall(x->x==1,requiredCh)
+	optOff=findall(x->x==0,Req[stepI,:])
+
+	m = Model(solver = GurobiSolver(PoolSearchMode=1,PoolSolutions=poolSol,PoolGap=0))
+	@variable(m,u[1:horzLen,1:N],Bin)
+	@variable(m,Test[1:horzLen])
+	@objective(m,Min,-sum(u))
+	@constraint(m,tempCon1,Test[1]>=evS.τP*prevT+evS.γP*(sum(u[1,n]*evS.imax[n] for n=1:N)+evS.iD_pred[stepI])^2+evS.ρP*evS.Tamb[stepI])
+	@constraint(m,tempCon2[kk=2:horzLen],Test[kk]>=evS.τP*Test[kk-1]+evS.γP*(sum(u[kk,n]*evS.imax[n] for n=1:N)+evS.iD_pred[stepI+kk])^2+evS.ρP*evS.Tamb[stepI+kk])
+	@constraint(m,Test.<=evS.Tmax)
+	# @constraint(m,optOn[kk=1:packLen],sum(u[n,kk] for n in requiredInd[kk])==length(requiredInd[kk]))
+	@constraint(m,optOnC[nn=1:length(requiredInd)],sum(u[kk,requiredInd[nn]] for kk=1:Int(min(abs(Req[stepI,requiredInd[nn]]),horzLen)))==min(abs(Req[stepI,requiredInd[nn]]),horzLen))
+	@constraint(m,optOffC[kk=1:horzLen],sum(u[kk,n] for n in optOff)==0)
+
+	TT = stdout
+	redirect_stdout()
+	statusC = solve(m)
+	redirect_stdout(TT)
+
+
+	if statusC==:Optimal
+		#take random solution if multiple
+		solCount=Gurobi.get_sol_count(getrawsolver(m))
+		if solCount>1
+			println(solCount," multiple solutions found")
+			solNum=rand(1:solCount-1)
+			setparam!(getrawsolver(m),"SolutionNumber",solNum)
+			getparam(getrawsolver(m),"SolutionNumber")
+			sol=Gurobi.get_dblattrarray(getrawsolver(m),"Xn",1,N)
+		else
+			sol=getvalue(u)[1,:]
+		end
+		Rec=sol
+	else
+		#return
+		Rec=requiredCh
+	end
+
+	#R>=1 and packRec get charged
+	for n=1:N
+		pemSol.Un[stepI,n]= if Rec[n]==1 evS.imax[n] else  0  end
+		prevSOC= if stepI>1 pemSol.Sn[stepI-1,n] else evS.s0[n] end
+		pemSol.Sn[stepI,n]=prevSOC+evS.ηP[n]*pemSol.Un[stepI,n]
+	end
+
+	return nothing
+end
+
+function pemEVCstep(stepI,evS,pemSol,silent)
+
+	packLen=10 #number of time steps
+
+	horzLen=min(packLen,evS.K-stepI)
+ 	Req=zeros(evS.K,N)
+
+	#println(stepI)
+	#send requests
+    for n=1:N
+		Req[stepI,n]=pemEVClocal(n,stepI,horzLen,packLen,evS,pemSol)
+	end
+
+	pemEVCcoord(stepI,horzLen,packLen,evS,pemSol,Req)
+
+	# actual dynamics
+	prevT = if stepI>1 pemSol.Tactual[stepI-1] else evS.t0 end
+	pemSol.uSum[stepI] = sum(pemSol.Un[stepI,n] for n=1:N)
+	pemSol.Itotal[stepI] = pemSol.uSum[stepI] + evS.iD_actual[stepI]
+	pemSol.Tactual[stepI] = evS.τP*prevT+evS.γP*pemSol.Itotal[stepI]^2+evS.ρP*evS.Tamb[stepI]
+	return nothing
+end
+
+function pemEVC(evS::scenarioStruct,slack::Bool,silent::Bool)
+	pemSol=solutionStruct(K=evS.K,N=evS.N,S=evS.S)
+
+	for stepI =1:evS.K-1
+		@printf "%s: time step %g of %g....\n" Dates.format(Dates.now(),"HH:MM:SS") stepI evS.K
+		try
+			pemEVCstep(stepI,evS,pemSol,silent)
+		catch e
+			@printf "error: %s" e
+			break
+		end
+	end
+
+	pemSol.objVal[1,1]=sum(sum((pemSol.Sn[stepI,n]-1)^2*evS.Qsi[n,1]+(pemSol.Un[stepI,n])^2*evS.Ri[n,1] for n=1:N) for stepI=1:(evS.K))
+	return pemSol
 end
