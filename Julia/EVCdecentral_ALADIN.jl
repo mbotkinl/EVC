05c0a8f2fd4ca4b6726c07e3144731c125455e4d
mbotkinl <32102213+mbotkinl@users.noreply.github.com>
2018-12-19 01:05:43 -0500

    Major updates: fix parallel and add iterative variables (itLam)
    working for ADMM needs testing for Dual and ALAD

--- Julia/EVCdecentral_ALADIN.jl
+++ Julia/EVCdecentral_ALADIN.jl
@@ -6,7 +6,7 @@
 #u, sn, xt, and z are all in "y", v is the auxilliary variable corresponding to x in literature
 #current constraint is coupling
 
-include("C://Users//micah//Documents//uvm//Research//EVC code//Julia//functions//funEVCpwl.jl")
+@everywhere include("C://Users//micah//Documents//uvm//Research//EVC code//Julia//functions//funEVCpwl.jl")
 eqString=if eqForm "_eq" else "_ineq" end
 fname = "dALADIN_N$(N)"*eqString
 
